aaa93c61d82fa158ae54d33e924f2b54c5ace1d6
Ignacio <ignaciobrenas@gmail.com>
2026-03-18 11:39:29 +0100

    RUN AND EAT (conexion a la base de datos)

---
+++ controller/bbdd.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$contrasena = "";
+$base_datos = "RUN_AND_EAT";
+
+$conexion = mysqli_connect($host, $usuario, $contrasena, $base_datos);
+
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
+mysqli_set_charset($conexion, "utf8");
+
+?>
